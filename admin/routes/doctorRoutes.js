10822170ddb379a2872b8c8acac5962d6e4874f9
swathi156 <swathiprabhu016@gmail.com>
2025-10-15 14:07:56 +0530

    Create doctorRoutes.js

---
+++ admin/routes/doctorRoutes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const doctorController = require("../controllers/doctorController");
+const authMiddleware = require("../middleware/authMiddleware");
+
+router.use(authMiddleware);
+router.get("/", doctorController.getAllDoctors);
+router.get("/:id", doctorController.getDoctor);
+router.put("/:id", doctorController.updateDoctor);
+router.delete("/:id", doctorController.deleteDoctor);
+
+module.exports = router;
