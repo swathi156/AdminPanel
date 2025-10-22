41b4b07fbdb2905f891b09ff40ba319ed4a210ee
swathi156 <swathiprabhu016@gmail.com>
2025-10-22 17:37:20 +0530

    Update server.js

--- admin/server.js
+++ admin/server.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const sequelize = require("./config/db");
-const { RolePermission, Role, User, Doctor } = require("./models");
+const { RolePermission, Role, User, Doctor, Patient } = require("./models");
 
 const authRoutes = require("./routes/authRoutes");
 const doctorRoutes = require("./routes/doctorRoutes");
-
+const patientRoutes = require("./routes/patientRoutes");
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
-app.get("/", (req,res)=>res.send("Server Running "));
+app.get("/", (req,res)=>res.send("Server Running"));
 
 app.use(authRoutes);
-app.use(doctorRoutes);
+app.use("/doctors",doctorRoutes);
+app.use(patientRoutes);
 
 async function syncDB(){
   try{
@@ -22,15 +23,15 @@ async function syncDB(){
     await Role.sync({alter:true});
     await User.sync({alter:true});
     await Doctor.sync({alter:true});
-   
-    console.log(" DB Synced");
+    await Patient.sync({alter:true});
+    console.log("DB Synced");
   }catch(err){console.error("DB Sync Error:",err);}
 }
 
 sequelize.authenticate()
-  .then(()=>console.log("DB Connected "))
-  .catch(err=>console.error("DB Error ",err));
+  .then(()=>console.log("DB Connected"))
+  .catch(err=>console.error("DB Error",err));
 
 syncDB();
-const PORT = process.env.PORT||5000;
-app.listen(PORT,()=>console.log(`Server running on port ${PORT} `));
+const PORT = process.env.PORT||5002;
+app.listen(PORT,()=>console.log(`Server running on port ${PORT} 🚀`));
